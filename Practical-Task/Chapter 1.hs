4e7d4a3057bd5d9004da1110075173bd7e2a8e37
Abdmubarak <mubarakabdulfatai17@gmail.com>
2025-12-11 18:26:10 +0100

    Update Chapter 1.hs

--- Practical-Task/Chapter 1.hs
+++ Practical-Task/Chapter 1.hs
@@ -13,3 +13,11 @@ main :: IO ()
 main = do
  print $ increment 60
  --Output = 61
+
+doubleThenincrement :: Int -> Int
+doubleThenincrement x = x * 2 + 1
+
+main :: IO ()
+main = do
+ print $ doubleThenincrement 60
+ --Output = 121
